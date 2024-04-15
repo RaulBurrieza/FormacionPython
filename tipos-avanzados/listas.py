0c4b641b7c7e2cfe3d83b7ea5e653739e614dd5e
RaulBurrieza <burriezaraul@gmail.com>
2024-04-15 16:02:23 +0200

    Se añade el archivo de las listas

---
+++ tipos-avanzados/listas.py
@@ -0,0 +1,17 @@
+numeros = [1, 2, 3]
+letras = ["a", "v", "b"]
+bools = [True, False, True]
+matriz = [[0, 1], [3, 1], [4, 5]]
+
+
+ceros = [0]*10
+
+
+alafanumerico = numeros + letras
+
+
+rango = list(range(1, 11))
+
+
+chars = list("hola mundo".replace(" ", ""))
+print(chars)
---
+++ tipos-avanzados/manipulando-listas.py
@@ -0,0 +1,13 @@
+mascotas = ["Rocky", "Pelusa", "Pulga"]
+print(mascotas[0])
+
+mascotas[0] = "Bicho"
+
+# print(mascotas)
+
+# print(mascotas[:2])
+# print(mascotas[-1])
+# print(mascotas[::2])  # SOLO ELEMENTOS PARES
+
+numeros = list(range(21))
+print(numeros[1::2])
